3856c349c6cd58518cac7d2e3e9696c43dd39262
Thibaut Molina <38030097+Tibo34@users.noreply.github.com>
2019-04-20 21:55:19 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import * as firebase from 'firebase';
 export class AppComponent {
   title = 'Blog';
 
+  /**
+  *config firebase
+  */
   constructor() {
     const config = {
-      apiKey: "AIzaSyCYb_HIxSgUZ6RQqBxc6PXs4bkPn4CIg7I",
-      authDomain: "blog-8652b.firebaseapp.com",
-      databaseURL: "https://blog-8652b.firebaseio.com",
-      projectId: "blog-8652b",
-      storageBucket: "blog-8652b.appspot.com",
-      messagingSenderId: "363315953741"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
     };
     firebase.initializeApp(config);
   }
